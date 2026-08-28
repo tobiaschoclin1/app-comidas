dc2df7ee822a82cf5ea15c438fe5c32f5e453acd
Tobias Choclin <tchoclin@uade.edu.ar>
2026-08-28 20:59:16 -0300

    Configurar Github Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
+const repoName = 'plan-alimentario'
+
 export default defineConfig({
   plugins: [react()],
+  base: process.env.NODE_ENV === 'production' ? `/${repoName}/` : '/',
 })
